3b40247ac3cf58280f757af30d20318554de52ad
nraufiero <129700842+nraufiero@users.noreply.github.com>
2023-10-11 19:57:22 +0000

    Pending changes exported from your codespace

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,29 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
         // TODO: implement function
-        return List.of();
+        Map<String, Integer> studentEvents = new HashMap<>(); 
+        for(String student : students) {
+                studentEvents.put(student,0);
+        }
+        for(String event : attendeesMapping.keySet()) {
+                for(String name : attendeesMapping.get(event)) {
+                        if(students.contains(name))
+                                studentEvents.put(name, studentEvents.get(name) + 1);
+                } 
+        }
+        return studentEvents.entrySet().stream()
+                .filter(e -> e.getValue() < 2)
+                .map(e -> e.getKey())
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
